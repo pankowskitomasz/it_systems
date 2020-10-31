965c62e6b1840d1681ba48fad2f1c0e2e3d4c3e7
pankowskitomasz <pankowski_tomasz@o2.pl>
2020-10-31 15:16:23 +0100

    project's initial config

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './scss/App.scss';
+import React,{Component} from 'react';
+import PropTypes from "prop-types";
+
+class App extends Component{
+  render(){
+    return(
+      <div title="IT Systems">
+
+      </div>
+    );
+  }
+}
+
+export default App;
